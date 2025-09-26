62dd16ccb4f833fcc6cdd263d7448ffcd4fa4250
RizzxDev <120876288+RizxxDev@users.noreply.github.com>
2025-09-26 15:39:44 +0700

    Update PlayPage.tsx

--- src/pages/PlayPage.tsx
+++ src/pages/PlayPage.tsx
@@ -60,7 +60,7 @@ export default function PlayPage() {
     if (user && progress) {
       const interval = setInterval(() => {
         saveProgress(localProgress);
-      }, 1000); // Save every 5 seconds
+      }, 1); // Save every 5 seconds
 
       return () => clearInterval(interval);
     }
